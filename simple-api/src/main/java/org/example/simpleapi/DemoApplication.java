eb94f7bad4defa64eefcfddaa08c54b5dd01d2e2
Fabian Bücheler <fabian.bucheler@getyourguide.com>
2024-09-30 21:47:30 +0200

    Add new GetMapping roominfo

--- simple-api/src/main/java/org/example/simpleapi/DemoApplication.java
+++ simple-api/src/main/java/org/example/simpleapi/DemoApplication.java
@@ -8,11 +8,19 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class DemoApplication {
+
+	private hotel hotel = new hotel();
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
+	}
+
+	@GetMapping("/roominfo")
+	public String roominfo(){
+		return String.format("Currently, there are %s rooms in the hotel.", hotel.getRoomnum());
 	}
 }
