09b081ba13c3016636bc9cfd4bc0be5e3aeaa78d
Fabian Bücheler <fabian.bucheler@getyourguide.com>
2024-10-04 20:23:27 +0200

    Create versatile-api project

---
+++ versatile-api/src/main/java/com/fb/versatile_api/VersatileApiApplication.java
@@ -0,0 +1,13 @@
+package com.fb.versatile_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersatileApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VersatileApiApplication.class, args);
+	}
+
+}
---
+++ versatile-api/src/test/java/com/fb/versatile_api/VersatileApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fb.versatile_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersatileApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
