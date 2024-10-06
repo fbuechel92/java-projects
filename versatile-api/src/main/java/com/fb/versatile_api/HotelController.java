61ba6c0b33afcbfbb21e4b6d82b2b3179b63231b
Fabian Bücheler <fabian.bucheler@getyourguide.com>
2024-10-06 18:22:30 +0200

    Add putmapping to the hotel controller

--- versatile-api/src/main/java/com/fb/versatile_api/HotelController.java
+++ versatile-api/src/main/java/com/fb/versatile_api/HotelController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,12 +14,12 @@ public class HotelController {
     private Hotel hotel = new Hotel();
 
     @GetMapping("/hotel")
-    public String getHotelInfo(){
+    public String getHotelInfo() {
         return String.format(
             "Hotel Name: %s.<br>" +
-            "Hotel Address: %s.<br>" +
-            "Contact: %s.<br><br>" +
-            "Total rooms: %s<br>",
+                "Hotel Address: %s.<br>" +
+                "Contact: %s.<br><br>" +
+                "Total rooms: %s<br>",
             hotel.getHotelName(),
             hotel.getHotelAddress(),
             hotel.getTelephoneNumber(),
@@ -31,24 +32,38 @@ public class HotelController {
         List<Room> rooms = hotel.listRooms();
         StringBuilder result = new StringBuilder("Rooms: <br>");
         for (Room room : rooms) {
-            result.append(String.format("Room %d: %d beds, $%.2f<br>", room.getRoomNumber(), room.getBedCount(), room.getRoomPrice()));
+            result.append(String.format("Room %d: %d beds, $%.2f<br>", room.getRoomNumber(), room.getBedCount(),
+                room.getRoomPrice()));
         }
         return result.toString();
     }
 
-    @PostMapping("/hotel/add")
-    public String addRoom(@RequestParam int roomNumber, @RequestParam int bedCount, @RequestParam double roomPrice){
+    @PostMapping("/hotel/addroom")
+    public String addRoom(@RequestParam int roomNumber, @RequestParam int bedCount, @RequestParam double roomPrice) {
         Room room = new Room(roomNumber, bedCount, roomPrice);
         hotel.addRoom(room);
         return String.format("Room %d successfully added", room.getRoomNumber());
     }
 
-    @DeleteMapping("/hotel/delete")
-    public String removeRoom(@RequestParam int roomNumber){
+    @DeleteMapping("/hotel/deleteroom")
+    public String removeRoom(@RequestParam int roomNumber) {
         Room room = hotel.findRoomByNumber(roomNumber);
         if (room != null) {
             hotel.removeRoom(room);
             return String.format("Room %d successfully removed", room.getRoomNumber());
+        }
+        return "Room not found";
+    }
+
+    @PutMapping("/hotel/changeroom")
+    public String changeRoom(@RequestParam int roomNumber, @RequestParam int bedCount, @RequestParam double roomPrice,
+                             @RequestParam boolean occupied) {
+        Room room = hotel.findRoomByNumber(roomNumber);
+        if (room != null) {
+            room.setRoomPrice(roomPrice);
+            room.setBedCount(bedCount);
+            room.setOccupied(occupied);
+            return "Room information successfully updated.";
         }
         return "Room not found";
     }
