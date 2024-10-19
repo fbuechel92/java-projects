f1e2f743614af5ddd32e303aa50bb28cda08c0f9
Fabian Bücheler <fabian.bucheler@getyourguide.com>
2024-10-19 23:06:45 +0200

    Create hotel management project

---
+++ hotel-management-app-v1/src/main/java/com/fb/hotelmanagementappv1/HotelManagementAppV1Application.java
@@ -0,0 +1,13 @@
+package com.fb.hotelmanagementappv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagementAppV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelManagementAppV1Application.class, args);
+    }
+
+}
