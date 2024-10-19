2383009fcb42374f1d3e2498d93a9ff1d42df198
Fabian Bücheler <fabianbuecheler@gmx.de>
2024-10-19 23:06:45 +0200

    Create hotel management project

---
+++ hotel-management-app-v1/src/test/java/com/fb/hotelmanagementappv1/HotelManagementAppV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fb.hotelmanagementappv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagementAppV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
