0ff4a5a57ce9aa9f22dde1a24fb3c0d49acc95ef
Fabian Bücheler <fabianbuecheler@gmx.de>
2024-10-20 15:44:25 +0200

    Add id variable to Room class

--- hotel-management-app-v1/src/main/java/com/fb/hotelmanagementappv1/Room.java
+++ hotel-management-app-v1/src/main/java/com/fb/hotelmanagementappv1/Room.java
@@ -1,12 +1,17 @@
 package com.fb.hotelmanagementappv1;
 
 public class Room {
+    private int id;
     private int roomSize;
     private int numberOfBeds;
     private boolean hasBalcony;
     private boolean hasTV;
 
     //Getter methods
+    private int getId(){
+        return id;
+    }
+
     private int getRoomSize(){
         return roomSize;
     }
