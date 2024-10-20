dba722b1b4a4da369f4309401a889a0a1f41c118
Fabian Bücheler <fabianbuecheler@gmx.de>
2024-10-20 15:43:41 +0200

    Add city class with instance variables and getter methods

---
+++ hotel-management-app-v1/src/main/java/com/fb/hotelmanagementappv1/City.java
@@ -0,0 +1,15 @@
+package com.fb.hotelmanagementappv1;
+
+public class City {
+    private int id;
+    private String name;
+
+    //Getter methods
+    private int getId(){
+        return id;
+    }
+
+    private String getName(){
+        return name;
+    }
+}
