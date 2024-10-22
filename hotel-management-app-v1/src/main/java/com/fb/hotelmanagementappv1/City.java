fba3067762049c1e3c032ca6b331d2ef44499d06
Fabian Bücheler <fabianbuecheler@gmx.de>
2024-10-22 20:31:54 +0200

    Remove lists from hotel and city class, to use repos instead

--- hotel-management-app-v1/src/main/java/com/fb/hotelmanagementappv1/City.java
+++ hotel-management-app-v1/src/main/java/com/fb/hotelmanagementappv1/City.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 public class City {
     private int id;
     private String name;
-    private List<Hotel> hotels;
 
     //Constructor
     public City(int id, String name){
@@ -22,9 +21,5 @@ public class City {
 
     private String getName(){
         return name;
-    }
-
-    private List<Hotel> getHotels(){
-        return hotels;
     }
 }
