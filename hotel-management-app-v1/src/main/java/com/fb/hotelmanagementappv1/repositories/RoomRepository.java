d4db69e08052ff1f46ad808402f157da068056b4
Fabian Bücheler <fabianbuecheler@gmx.de>
2024-10-26 16:38:06 +0200

    Add room repository with CRUD methods

---
+++ hotel-management-app-v1/src/main/java/com/fb/hotelmanagementappv1/repositories/RoomRepository.java
@@ -0,0 +1,32 @@
+package com.fb.hotelmanagementappv1.repositories;
+
+import com.fb.hotelmanagementappv1.models.Room;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RoomRepository {
+    private List<Room> rooms;
+
+    //Constructor
+    public RoomRepository(){
+        this.rooms = new ArrayList<>();
+    }
+
+    //Methods
+    public void addRoom(Room paramRoom){
+        rooms.add(paramRoom);
+    }
+
+    public Room getRoom(int id){
+        for (Room room:rooms){
+            if (room.getId() == id){
+                return room;
+            }
+        }
+        return null;
+    }
+
+    public List<Room> getAllRooms(){
+        return rooms;
+    }
+}
