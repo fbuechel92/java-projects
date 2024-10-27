10fa1c98fc98713f444bd15d524e533138fb0819
Fabian Bücheler <fabianbuecheler@gmx.de>
2024-10-27 12:43:39 +0100

    Add booking and cancellation methods to the UserService

--- hotel-management-app-v1/src/main/java/com/fb/hotelmanagementappv1/services/UserService.java
+++ hotel-management-app-v1/src/main/java/com/fb/hotelmanagementappv1/services/UserService.java
@@ -42,6 +42,30 @@ public class UserService {
         return roomRepository.getRoomsByHotelId(hotelId);
     }
 
+    //Booking related Methods
+    public String bookRoom(int roomId) {
+        Room room = roomRepository.getRoom(roomId);
 
+        if (room == null) {
+            return String.format("Room %d does not exist.", roomId);
+        }
+        if (room.isOccupied() == true) {
+            return String.format("Room %d is already occupied.", roomId);
+        }
+        roomRepository.updateOccupied(roomId, true);
+        return String.format("Room %d has been successfully booked.", roomId);
+    }
 
+    public String cancelRoom(int roomId) {
+        Room room = roomRepository.getRoom(roomId);
+
+        if (room == null) {
+            return String.format("Room %d does not exist.", roomId);
+        }
+        if (room.isOccupied() == false) {
+            return String.format("Room %d is not booked. Cancellation not successful.", roomId);
+        }
+        roomRepository.updateOccupied(roomId, false);
+        return String.format("Cancellation for room %d has been cancelled.", roomId);
+    }
 }
