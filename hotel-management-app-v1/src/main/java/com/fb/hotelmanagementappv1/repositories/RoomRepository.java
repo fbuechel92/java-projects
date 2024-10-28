97ab42f3911d16d151f1e943e110fc76e344d98a
Fabian Bücheler <fabianbuecheler@gmx.de>
2024-10-28 17:50:42 +0100

    Add Room Repository

---
+++ hotel-management-app-v1/src/main/java/com/fb/hotelmanagementappv1/repositories/RoomRepository.java
@@ -0,0 +1,53 @@
+package com.fb.hotelmanagementappv1.repositories;
+
+import com.fb.hotelmanagementappv1.models.Room;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RoomRepository {
+    private List<Room> rooms;
+
+    //Constructor
+    public RoomRepository(){
+        this.rooms = new ArrayList<>();
+    }
+
+    //Methods
+    public void addRoom(Room paramRoom){
+        rooms.add(paramRoom);
+    }
+
+    public Room getRoom(int id){
+        for (Room room:rooms){
+            if (room.getId() == id){
+                return room;
+            }
+        }
+        return null;
+    }
+
+    public List<Room> getAllRooms(){
+        return rooms;
+    }
+
+    public List<Room> getRoomsByHotelId(int paramHotelId){
+        List<Room> roomList = new ArrayList<>();
+
+        for (Room room:rooms){
+            if (room.getHotelId() == paramHotelId){
+                roomList.add(room);
+            }
+        }
+        return roomList;
+    }
+
+    public void updateOccupied(int roomId, boolean paramOccupied){
+        for (Room room:rooms){
+            if (room.getId() == roomId){
+                room.setOccupied(paramOccupied);
+            }
+        }
+    }
+}
