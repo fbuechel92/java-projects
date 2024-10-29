578483f8bae4551c76adbc18d3320500e14826a6
Fabian Bücheler <fabianbuecheler@gmx.de>
2024-10-29 19:34:57 +0100

    Add request mapping to management controller

--- hotel-management-app-v1/src/main/java/com/fb/hotelmanagementappv1/controllers/ManagementController.java
+++ hotel-management-app-v1/src/main/java/com/fb/hotelmanagementappv1/controllers/ManagementController.java
@@ -11,9 +11,11 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/admin")
 public class ManagementController {
     private ManagementService managementService;
 
